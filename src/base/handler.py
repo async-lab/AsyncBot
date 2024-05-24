e0ef6767f84ff1b314d6c8b529dc3e5391daa2c5
dsx137 <1419696375@qq.com>
2024-05-24 11:13:35 +0800

    fix

--- src/base/handler.py
+++ src/base/handler.py
@@ -319,3 +319,4 @@ def init_handlers():
             msg_id=votes[0]["msg_id"], content=json.dumps(await get_vote_message())
         )
         program.db.delete("vote", "1=1")
+        message.reply("投票已结束")
